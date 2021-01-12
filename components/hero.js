cbfcc26f0af6ffe9e636396c960a03c7d586cdc4
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2021-01-12 20:19:03 +0100

    Updated hero

--- components/hero.js
+++ components/hero.js
@@ -2,11 +2,22 @@ import { SITE_TITLE, SITE_SUBTITLE } from '@lib/constants'
 
 export default function Hero() {
   return (
-    <section className="hero">
+    <section className="hero is-primary">
       <div className="hero-body">
         <div className="container is-max-desktop has-text-centered">
           <h1 className="title is-size-1">{SITE_TITLE}</h1>
           <h2 className="subtitle is-size-4">{SITE_SUBTITLE}</h2>
+          <div className="columns has-text-centered">
+            <div className="column">
+              <a aria-label="phone" href="tel:+11234567890">+11234567890</a>
+            </div>
+            <div className="column">
+              <a aria-label="email" href="mailto:carl.sagan@gmail.com">carl.sagan@gmail.com</a>
+            </div>
+            <div className="column">
+              <a aria-label="LinkedIn" href="https://www.linkedin.com/in/carlsagan/">LinkedIn</a><span> | </span><a aria-label="Twitter" href="https://twitter.com/carlsagan">Twitter</a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- pages/index.js
+++ pages/index.js
@@ -6,43 +6,33 @@ export default function Index() {
     <>
       <Layout>
         <Container>
-          <h3 className="title">Contacts</h3>
-          <div className="columns has-text-centered has-text-left-mobile">
+          <div className="columns">
             <div className="column">
-              <a aria-label="phone" href="tel:+11234567890">+11234567890</a>
+              <h3 className="title">About</h3>
+              <div className="block">
+                American astronomer, planetary scientist, cosmologist, astrophysicist, astrobiologist, author, poet, and science communicator.
+              </div>
+              <div className="block">
+                Best known scientific contribution is research on extraterrestrial life, including experimental demonstration of the production of amino acids from basic chemicals by radiation.
+              </div>
             </div>
             <div className="column">
-              <a aria-label="email" href="mailto:carl.sagan@gmail.com">carl.sagan@gmail.com</a>
+              <h3 className="title">Skills</h3>
+              <div className="tags are-medium">
+                <span className="tag">Mathematics</span>
+                <span className="tag">Astrophysics</span>
+                <span className="tag">Cosmology</span>
+                <span className="tag">Astrobiology</span>
+              </div>
+              <div className="tags are-medium">
+                <span className="tag">Science Communication</span>
+              </div>
+              <div className="tags are-medium">
+                <span className="tag">Creativity</span>
+                <span className="tag">Passion</span>
+                <span className="tag">Inquisitivity</span>
+              </div>
             </div>
-            <div className="column">
-              <a aria-label="LinkedIn" href="https://www.linkedin.com/in/carlsagan/">LinkedIn</a><span> | </span><a aria-label="Twitter" href="https://twitter.com/carlsagan">Twitter</a>
-            </div>
-          </div>
-        </Container>
-        <Container>
-          <h3 className="title">About</h3>
-          <div className="block">
-            American astronomer, planetary scientist, cosmologist, astrophysicist, astrobiologist, author, poet, and science communicator.
-          </div>
-          <div className="block">
-            Best known scientific contribution is research on extraterrestrial life, including experimental demonstration of the production of amino acids from basic chemicals by radiation.
-          </div>
-        </Container>
-        <Container>
-          <h3 className="title">Skills</h3>
-          <div className="tags are-medium">
-            <span className="tag">Mathematics</span>
-            <span className="tag">Astrophysics</span>
-            <span className="tag">Cosmology</span>
-            <span className="tag">Astrobiology</span>
-          </div>
-          <div className="tags are-medium">
-            <span className="tag">Science Communication</span>
-          </div>
-          <div className="tags are-medium">
-            <span className="tag">Creativity</span>
-            <span className="tag">Passion</span>
-            <span className="tag">Inquisitivity</span>
           </div>
         </Container>
         <Container>
@@ -57,14 +47,36 @@ export default function Index() {
               <div className="content">
                 <div className="columns">
                   <div className="column">
-                    <h4 className="title">University of California, Berkeley</h4>
-                    <p>Miller Fellow</p>
+                    <h4 className="title">Awesome Ltd</h4>
+                    <p>Senior Astrophysicist</p>
                   </div>
                   <div className="column has-text-right has-text-left-mobile">
-                    <p className="is-italic">1960 - 1962</p>
+                    <p className="is-italic">May 2019 - Present</p>
                   </div>
                 </div>
-                <p>American astronomer, planetary scientist, cosmologist, astrophysicist, astrobiologist, author, poet, and science communicator.</p>
+                <p>Accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo. Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam. Facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
+              </div>
+            </div>
+          </article>
+          <article className="media">
+            <figure className="media-left is-hidden-mobile">
+              <p className="image is-64x64">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </p>
+            </figure>
+            <div className="media-content">
+              <div className="content">
+                <div className="columns">
+                  <div className="column">
+                    <h4 className="title">Great Company Ltd</h4>
+                    <p>Cosmologist</p>
+                  </div>
+                  <div className="column has-text-right has-text-left-mobile">
+                    <p className="is-italic">Sep 2015 - May 2019</p>
+                  </div>
+                </div>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit. Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam. Laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>At vero eos et accusamus. Eaque ipsa quae ab illo inventore veritatis et quasi. Accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem.</p>
               </div>
             </div>
           </article>
