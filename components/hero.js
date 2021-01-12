b736596d41119b6b4364e593c58a30a47c005071
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2021-01-12 18:30:28 +0100

    Updated hero

--- components/hero.js
+++ components/hero.js
@@ -5,8 +5,8 @@ export default function Hero() {
     <section className="hero">
       <div className="hero-body">
         <div className="container is-max-desktop has-text-centered">
-          <h1 className="title">{SITE_TITLE}</h1>
-          <h2 className="subtitle">{SITE_SUBTITLE}</h2>
+          <h1 className="title is-size-1">{SITE_TITLE}</h1>
+          <h2 className="subtitle is-size-4">{SITE_SUBTITLE}</h2>
         </div>
       </div>
     </section>
