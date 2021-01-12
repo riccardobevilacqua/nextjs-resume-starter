862d5e589bd3cfd7c8d4e927536d2919bd1dd71e
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2021-01-12 18:05:04 +0100

    Fixed hero

--- components/hero.js
+++ components/hero.js
@@ -1,20 +1,14 @@
-import Link from 'next/link'
-
 import { SITE_TITLE, SITE_SUBTITLE } from '@lib/constants'
 
 export default function Hero() {
   return (
     <section className="hero">
-      <Link href="/">
-        <a>
-          <div className="hero-body">
-            <div className="container is-max-desktop">
-              <h1 className="title">{SITE_TITLE}</h1>
-              <h2 className="subtitle">{SITE_SUBTITLE}</h2>
-            </div>
-          </div>
-        </a>
-      </Link>
+      <div className="hero-body">
+        <div className="container is-max-desktop has-text-centered">
+          <h1 className="title">{SITE_TITLE}</h1>
+          <h2 className="subtitle">{SITE_SUBTITLE}</h2>
+        </div>
+      </div>
     </section>
   )
 }
