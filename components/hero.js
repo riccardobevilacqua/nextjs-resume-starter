c8c632e47cfef9914c1a56f3d8140cea890335b7
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2021-01-14 13:14:11 +0100

    Updated hero layout

--- components/hero.js
+++ components/hero.js
@@ -3,21 +3,33 @@ import styles from '../styles/Hero.module.scss';
 
 export default function Hero() {
   return (
-    <div className={[styles.hero, 'grid'].join(' ')}>
+    <div className={`${styles.hero} grid`}>
       <div className={[styles.heroHeader, 'cs1 ce12'].join(' ')}>
         <h1>{SITE_TITLE}</h1>
         <h2>{SITE_SUBTITLE}</h2>
       </div>
       <div className="cs1 ce4">
-        <a aria-label="phone" href="tel:+11234567890">+11234567890</a>
+        <a aria-label="phone" href="tel:+11234567890">
+          <i className="eva eva-phone"></i>
+          <span>+11234567890</span>
+        </a>
       </div>
       <div className="cs5 ce8">
-        <a aria-label="email" href="mailto:carl.sagan@gmail.com">carl.sagan@gmail.com</a>
+        <a aria-label="email" href="mailto:carl.sagan@gmail.com">
+          <i className="eva eva-email"></i>
+          <span>carl.sagan@gmail.com</span>
+        </a>
       </div>
-      <div className="cs9 ce12">
-        <a aria-label="LinkedIn" href="https://www.linkedin.com/in/carlsagan/">LinkedIn</a>
-        <span style={{ padding: '0 0.5em' }}>|</span>
-        <a aria-label="Twitter" href="https://twitter.com/carlsagan">Twitter</a>
+      <div className={`${styles.heroSocial} cs9 ce12`}>
+        <a aria-label="LinkedIn" href="https://www.linkedin.com">
+          <i className="eva eva-linkedin"></i>
+        </a>
+        <a aria-label="Twitter" href="https://twitter.com">
+          <i className="eva eva-twitter"></i>
+        </a>
+        <a aria-label="GitHub" href="https://github.com">
+          <i className="eva eva-github"></i>
+        </a>
       </div>
     </div>
   )
